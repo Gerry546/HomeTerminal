583d9dcd573adc0f5362bf7ff6db70131fa26d4d
Tom Geelen <t.f.g.geelen@gmail.com>
2022-05-23 23:12:07 +0200

    added initialization scripts

---
+++ scripts/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e 
+
+scriptsDirectory=$(realpath $(dirname $0))
+rootDirectory=$(realpath "${scriptsDirectory}/..")
+buildDirectory=$rootDirectory/build
+
+rm -rf ${buildDirectory}/conf/
+
+if [ ! -d ${buildDirectory}/conf ] 
+then
+    echo "Creating build directory"
+    mkdir -p ${buildDirectory}/conf
+fi
+
+if [ ! -f ${buildDirectory}/conf/local.conf ] 
+then
+    echo "Setting up prepared local.conf"
+    ln -s ${scriptsDirectory}/templates/bblayers.conf ${buildDirectory}/conf/bblayers.conf
+    ln -s ${scriptsDirectory}/templates/local.conf ${buildDirectory}/conf/local.conf
+fi
+
+. ${rootDirectory}/openembedded-core/oe-init-build-env ${buildDirectory}
+echo "Initialization done"
